67761deca7dc5ee59b21aac4e93ee0f1882f7f72
KadyrPoyraz228337 <poyraz_2001@mail.ru>
2020-02-22 18:37:58 +0600

    Обернул запросы в try catch

--- src/store/action/news.js
+++ src/store/action/news.js
@@ -9,34 +9,60 @@ export const postAction = data => ({type: GET_POST, data});
 export const getCommentsAction = data => ({type: GET_COMMENTS, data});
 
 export const getPosts = () => async dispatch => {
-    const resp = await axios.get('http://localhost:8000/news');
-    dispatch(postsAction(resp.data));
+    try {
+        const resp = await axios.get('http://localhost:8000/news');
+        dispatch(postsAction(resp.data));
+    } catch (e) {
+        console.log(e);
+    }
 };
 
 export const getPost = id => async dispatch => {
-    console.log('http://localhost:8000/news/'+id);
-    const resp = await axios.get('http://localhost:8000/news/'+id);
-    dispatch(postAction(resp.data));
+    try {
+        const resp = await axios.get('http://localhost:8000/news/'+id);
+        dispatch(postAction(resp.data));
+    } catch (e) {
+        console.log(e);
+    }
 };
 
 export const getComments = id => async dispatch => {
-    const resp = await axios.get('http://localhost:8000/comments?news_id='+id);
-    dispatch(getCommentsAction(resp.data));
+    try {
+        const resp = await axios.get('http://localhost:8000/comments?news_id='+id);
+        dispatch(getCommentsAction(resp.data));
+    } catch (e) {
+        console.log(e);
+    }
 };
 
 export const addComment = data => async dispatch => {
-    await axios.post('http://localhost:8000/comments', data);
+    try {
+        await axios.post('http://localhost:8000/comments', data);
+    } catch (e) {
+        console.log(e);
+    }
 };
 
 export const deleteComment = id => async dispatch => {
-    await axios.delete('http://localhost:8000/comments/'+id);
+    try {
+        await axios.delete('http://localhost:8000/comments/'+id);
+    } catch (e) {
+        console.log(e);
+    }
 };
 
 export const addNews = data => async dispatch => {
-    await axios.post('http://localhost:8000/news', data);
+    try {
+        await axios.post('http://localhost:8000/news', data);
+    } catch (e) {
+        console.log(e);
+    }
 };
 
 export const deleteNews = id => async dispatch => {
-    console.log(id);
-    await axios.delete('http://localhost:8000/news/'+id);
+    try {
+        await axios.delete('http://localhost:8000/news/'+id);
+    } catch (e) {
+        console.log(e);
+    }
 };
